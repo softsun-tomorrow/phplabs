9f9943964da477a3d2fb5748693149c0e5bf3640
liutengfei <tengfei.liu@handeson.com>
2020-04-22 17:46:22 +0800

    feat:修改

--- insertartist.php
+++ insertartist.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$conn=mysqli_connect("db.soic.indiana.edu","i308_data","my+sql=i308_data","i308_dataset");
+$conn=mysqli_connect("118.25.5.174","liutengfei","ltf107834","i308_dataset");
 
 // Check connection
 if (!$conn) {
@@ -9,38 +9,49 @@ if (!$conn) {
 
 // Grab POST Data
 //escape variables for security sql injection
-$sanbtitle = mysqli_real_escape_string($conn, $_POST['form_bandtitle']);
-$sanyearf = mysqli_real_escape_string($conn, $_POST['form_yearformed']);
+$fname = mysqli_real_escape_string($conn, $_POST['form_fname']);
+$lname = mysqli_real_escape_string($conn, $_POST['form_lname']);
+$dob = mysqli_real_escape_string($conn, $_POST['form_dob']);
+$hometown = mysqli_real_escape_string($conn, $_POST['form_hometown']);
+$gender = mysqli_real_escape_string($conn, $_POST['form_gender']);
 
+
+if($fname){
+    $where = "where fname = '".$fname."' and lname = '".$lname."' ";
+}else{
+    $where = '';
+}
+
+$sql = "SELECT * from artist12 ".$where;
+$result = mysqli_query($conn, $sql);
+$num_rows = mysqli_num_rows($result);
+if ($num_rows > 0) {
+    echo "The First Name and Last Name already exists<br>";
+    // Close Connection
+    mysqli_close($conn);
+    exit;
+}
 //Insert query to insert form data into the band table
-$sql = "INSERT INTO band12 (title, year_formed) VALUES ('$sanbtitle','$sanyearf')";
+$sql = "INSERT INTO artist12 (fname, lname, dob, hometown, gender) VALUES ('$fname','$lname','$dob','$hometown','$gender')";
 //check for error on insert
-if (!mysqli_query($con,$sql))
-{ die('Error: ' . mysqli_error($con)) . "<br>"; }
+if (!mysqli_query($conn,$sql)) {
+    die('Error: ' . mysqli_error($conn)) . "<br>";
+}
 
-echo "Artist Added <br>";
-
-
-
+echo "Artist Added <br><br><br><br>";
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-/**
+if($fname){
+    $where = "where fname = '".$fname."' and lname = '".$lname."' ";
+}else{
+    $where = '';
+}
 
 // Create SQL Statement
-$sql = "SELECT * from emp_shift where role = '$var_role'";
+$sql = "SELECT * from artist12 ".$where;
 
 // Get Results
 $result = mysqli_query($conn, $sql);
@@ -51,19 +62,20 @@ $num_rows = mysqli_num_rows($result);
 // Display Results
 if ($num_rows > 0) {
     echo "<table>";
-	echo "<tr><th>Shift ID</th><th>Employee ID</th><th>wdate</th><th>time_in</th><th>time_out</th><th>role</th></tr>";
-    // Output data of each row, ->fetch_assoc gives array of arrays with keys matching column names
+    echo "<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Dob</th><th>Hometown</th><th>gender</th></tr>";
+// Output data of each row, ->fetch_assoc gives array of arrays with keys matching column names
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["shiftid"]."</td><td>".$row["empid"]."</td><td>".$row["wdate"]."</td>
-		      <td>".$row["time_in"]."</td><td>".$row["time_out"]."</td><td>".$row["role"]."</td></tr>";
+        echo "<tr><td>".$row["id"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td>
+<td>".$row["dob"]."</td><td>".$row["hometown"]."</td><td>".$row["gender"]."</td></tr>";
     }
     echo "</table>";
 } else {
     echo "0 results <br>";
 }
 echo "$num_rows Rows <br>";
-**/
 
 // Close Connection
 mysqli_close($conn);
-?>
+
+
+exit;
